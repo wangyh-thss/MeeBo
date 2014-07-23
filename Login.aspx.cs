a36a26a3d198e1c459d11f49e5294892e0b0eb17
wangyh-thss <freedom6558@gmail.com>
2014-07-23 15:29:44 +0800

    logic - login and register

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -4,11 +4,52 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using MeeBoDB;
 
 public partial class Login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if(IsPostBack)
+        {
+            string uPwd = Request.Form["form-password"];
+            string uName = Request.Form["form-user"];
+            DataTable result = searchByUserName(uName);
+            if (result.Rows.Count > 0)
+            {
+                //用户名正确 检验密码和身份
+                foreach (DataRow user in result.Rows)
+                {
+                    if (user["UPassword"] == uPwd)
+                    {
+                        if (user["UAdmin"] == 0)
+                        {
+                            //普通用户登录
+                            Session["role"] = "admin";
+                            Session["name"] = uName;
+                            Response.Redirect("~/UserInfo.aspx");
+                        }
+                        else
+                        {
+                            //管理员登录
+                            Session["role"] = "user";
+                            Session["name"] = uName;
+                            Response.Redirect("~/UserInfo.aspx");
+                        }
+                    }
+                    else
+                    {
+                        //用户名存在，密码错误·
+                        this.error_label.InnerText = "密码错误";
+                    }
+                }
+            }
+            else
+            {
+                //用户名密码错误
+                this.error_label.InnerText = "用户不存在";
+            }
+        }
     }
 }
--- Register.aspx.cs
+++ Register.aspx.cs
@@ -4,11 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 public partial class Register : System.Web.UI.Page
 {
+    User user = new User();
     protected void Page_Load(object sender, EventArgs e)
     {
-
     }
+
+    protected void btnRegister_Click(object sender, EventArgs e)
+    {
+        if (name.Text == string.Empty)
+        {
+            name_error.innerText = "用户名不能为空";
+        }
+        if (password.Text == string.Empty)
+        {
+            password_error.innerText = "密码不能为空";
+        }
+        if (confirm.Text == string.Empty)
+        {
+            confirm_error.innerText = "两次输入密码不同";
+        }
+        string uPwd = Request.Form["form-password"];
+        string uName = Request.Form["form-user"];
+        DataTable result = searchByUserName(uName);
+        if (result.Rows.Count > 0)
+        {
+            name_error.innerText = "用户名已被注册";
+            return;
+        }
+        user.name = uName;
+        user.password = uPwd;
+        user.nickName = Request.Form["form-nickName"];
+        user.admin = 0;
+        user.email = Request.Form["form-emaim"];
+        user.fansNum = 0;
+        user.likesNum = 0;
+        user.newsNum = 0;
+        user.savesNewsNum = 0;
+        user.msgInNum = 0;
+        user.msgOutNum = 0;
+        Insert(user);
+    }
+
 }
