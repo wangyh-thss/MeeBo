c15b9585d3e93ac30def2d5ecfdf45591184512a
钱珺 <jy8239778@126.com>
2014-07-27 16:25:10 +0800

    收藏数据库

--- App_Code/PraiseDB.cs
+++ App_Code/PraiseDB.cs
@@ -23,5 +23,62 @@ namespace MeeboDb
             // TODO: 在此处添加构造函数逻辑
             //
         }
+
+        public Guid ID { get; set; }
+        public Guid UserID { get; set; }
+        public Guid NewsID { get; set; }
+        public Boolean isCheck { get; set; }
+        public Guid NewsUserID { get; set; }
+
+        DataBase data = new DataBase();
+
+        public int SearchNumber;
+        UserDB thisUser = new UserDB();
+        NewsDB thisNews = new NewsDB();
+
+        public Guid Insert()
+        {
+            DataSet ds = data.GetData("select * from [Praise]", "thisPraise");
+            DataRow row = ds.Tables["thisPraise"].NewRow();
+            ID = Guid.NewGuid();
+            row["PID"] = ID;
+            row["PUID"] = UserID;
+            row["PNID"] = NewsID;
+            row["PNUID"] = NewsUserID;
+            ds.Tables["thisPraise"].Rows.Add(row);
+            thisNews.ChangeProNum(NewsID, 1);
+            data.UpdateData("select * from [Praise] ", ds, "thisPraise");
+            return ID;
+        }
+
+        public void Delete(Guid thisID)
+        {
+            SqlParameter[] prams = 
+            {
+			    data.MakeInParam("@PID",SqlDbType.UniqueIdentifier,16,thisID),
+			};
+            DataSet ds = data.GetData("select * from [Praise] where PID = @PID", prams, "thisPraise");
+            if (ds.Tables["thisPraise"].Rows.Count == 1)
+            {
+                thisUser.ChangeSaveNewsNum(thisID, -1);
+                thisNews.ChangeSaveNum(thisID, -1);
+                ds.Tables["thisPraise"].Clear();
+            }
+            data.UpdateData("select * from [Praise] where PID = @PID", prams, ds, "thisPraise");
+        }
+
+        public void Delete(Guid thisUserID, Guid thisNewsID)
+        {
+ 
+        }
+
+        public void DeleteByUser(Guid UserID)
+        {
+        }
+
+        public void DeleteByNews(Guid NewsID)
+        {
+        }
+
     }
 }
